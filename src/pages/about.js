1b58c46197afbb5488e077fbf4280a9ac86d4ed0
COX <trung2222008@gmail.com>
2023-05-24 07:38:52 +0700

    Add existing project files to Git

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import Layout from "../components/layout"
+const AboutPage = () => {
+    return(
+<Layout pageTitle = "About Page">
+    <p>This is my About Page!</p>
+</Layout>
+    )
+}
+
+export const Head = () => <title>About Page</title>
+
+export default AboutPage
